d1d949ae1aebd5595f995ef0f413f75beaaa80fa
shun <shun.syukatsu@gmail.com>
2023-12-27 02:51:50 +0900

    add directory db and web

---
+++ db/mysql_setup.sql
@@ -0,0 +1,11 @@
+/*
+db フォルダ内に mysql_setup.sql ファイルを新規作成し、下記の内容で保存します。
+ここでは、tutorial_develop というデータベースを作成し、
+どこからでもアクセス可能ですべての権限を持つユーザ devuser を作成します
+（rootなどデフォルトで存在するユーザは、ローカルPCからしかログインできません）。
+*/
+
+CREATE DATABASE IF NOT EXISTS daily_report_system CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE USER IF NOT EXISTS 'repuser'@'%' IDENTIFIED BY 'reppass';
+GRANT ALL PRIVILEGES ON *.* TO 'repuser'@'%';
+CREATE DATABASE IF NOT EXISTS wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
