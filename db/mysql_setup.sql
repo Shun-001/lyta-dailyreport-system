beb5dafd168e8ea21314b2a8c28726a698c12881
shun <shun.syukatsu@gmail.com>
2023-12-27 05:32:40 +0900

    updated

--- db/mysql_setup.sql
+++ db/mysql_setup.sql
@@ -8,4 +8,6 @@ db フォルダ内に mysql_setup.sql ファイルを新規作成し、下記の
 CREATE DATABASE IF NOT EXISTS daily_report_system CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 CREATE USER IF NOT EXISTS 'repuser'@'%' IDENTIFIED BY 'reppass';
 GRANT ALL PRIVILEGES ON *.* TO 'repuser'@'%';
+/*
 CREATE DATABASE IF NOT EXISTS wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+*/
