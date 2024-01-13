77f6224ffe41f2170d58484b12a67f0a0cfb7892
shun <shun.syukatsu@gmail.com>
2024-01-13 19:43:44 +0900

    edited

--- src/main/java/com/techacademy/repository/ProjectRepository.java
+++ src/main/java/com/techacademy/repository/ProjectRepository.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.techacademy.entity.Employee;
 import com.techacademy.entity.Report;
 
 public interface ProjectRepository extends JpaRepository<Report, Integer> {
 
-    List<Report> findByEmployee_code(String code);
+    List<Employee> findByCode(String code);
 
 }
